89a037a4c57c23f0621e3e8d359f479f53b1c593
unknown <fauzangustifa@gmail.com>
2023-01-06 23:35:48 +0700

    Add Kelompok Mapel

---
+++ app/Http/Controllers/KelompokMapel/KelompokMapelController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\KelompokMapel;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class KelompokMapelController extends Controller
+{
+    //
+}
